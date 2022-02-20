a4e118a7bef07885e1ce45141cd1ddac7969a656
yovanproev <yovanproev@yahoo.com>
2022-02-20 20:19:18 +0100

    lecture2 commit

---
+++ Week1/Exercise1/E3/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Enter Text: ")
+		// reads user input until \n by default
+		scanner.Scan()
+		// Holds the string that was scanned
+		text := scanner.Text()
+		if len(text) != 0 {
+			fmt.Println(text)
+		} else {
+			// exit if user entered an empty string
+			break
+		}
+
+	}
+
+	// handle error
+	if scanner.Err() != nil {
+		fmt.Println("Error: ", scanner.Err())
+	}
+}
