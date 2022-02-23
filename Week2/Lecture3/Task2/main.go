6f99a0d06f5b15ce85164eeff90d8c92df245bd0
yovanproev <yovanproev@yahoo.com>
2022-02-23 14:01:11 +0100

    Lecture3 final commit

--- Week2/Lecture3/Task2/main.go
+++ Week2/Lecture3/Task2/main.go
@@ -32,9 +32,21 @@ func groupSlice(keySlice []string, valueSlice []int) map[string][]int {
 	keys := make(map[string][]int)
 
 	for i, city := range keySlice {
-		keys[city] = valueSlice[:4]
+		var limitingSlice int
+		if i < valueSlice[i] {
+			limitingSlice = valueSlice[i] - i
+			keys[city] = valueSlice[i : valueSlice[i]-limitingSlice+4]
+		} else {
+			limitingSlice = i - valueSlice[i]
+			keys[city] = valueSlice[valueSlice[i] : i-limitingSlice+4]
+		}
 		i++
 	}
-	fmt.Println(keys)
+	fmt.Print(keys)
 	return keys
 }
+
+//Output:
+// map[Berlin:[96 65 56 30] Chicago:[29 41 98 94] London:[89 35 75 23] Moscow:[29 41 98 94] Tokyo:[16 49 3 73]]
+// map[Berlin:[21 35 9 62] Chicago:[87 36 37 27] London:[84 69 22 67] Moscow:[20 56 68 60] Tokyo:[27 5 15 91]]
+// map[Berlin:[41 62 27 78] Chicago:[80 25 15 73] London:[5 90 71 59] Moscow:[90 71 59 63] Tokyo:[15 9 88 72]]
