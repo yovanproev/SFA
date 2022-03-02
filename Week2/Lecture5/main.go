9fe06e781c50317b0f0ac3cba50dbe747e6bb2f9
yovanproev <yovanproev@yahoo.com>
2022-03-02 16:06:08 +0100

    updated homework Lecture 5

---
+++ Week2/Lecture5/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	compareCards "compareCards/Tasks"
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Enter Card 1, Card 1 suit, Card 2, Card 2 suit: ")
+	var card1, cardSuit1, card2, cardSuit2 string
+
+	_, err := fmt.Scanf("%s %s %s %s", &card1, &cardSuit1, &card2, &cardSuit2)
+
+	if err != nil {
+		panic(err)
+	}
+
+	//Task 1
+	var Cards = compareCards.ConvertCardsToNumber(card1, cardSuit1, card2, cardSuit2)
+	fmt.Println(compareCards.CompareCards(Cards, Cards))
+
+	// Task 2
+	compareCards.MaxCard(compareCards.CardsDeck.Cards)
+}
