90b48ceadd8b2eb1bd6633e8ab55e117763eb1ed
yovanproev <yovanproev@yahoo.com>
2022-03-04 10:19:12 +0100

    update

--- Week3/Lecture7/Task1/main.go
+++ Week3/Lecture7/Task1/main.go
@@ -20,7 +20,7 @@ type Card struct {
 func main() {
 	deck := makeDeck()
 	deck.shuffle()
-	deal(deck, 52)
+	deck.deal(10)
 }
 
 func makeDeck() Deck {
@@ -38,12 +38,12 @@ func makeDeck() Deck {
 	return cards
 }
 
-func deal(d Deck, handSize int) {
+func (d Deck) deal(handSize int) {
 	if handSize == 52 {
 		fmt.Println("No more cards in the deck")
 	}
 	for i := 0; i < handSize; i++ {
-		fmt.Println(d.Cards[i])
+		fmt.Print(d.Cards[i])
 	}
 }
 
@@ -57,3 +57,10 @@ func (d Deck) shuffle() {
 		d.Cards[i], d.Cards[newPosition] = d.Cards[newPosition], d.Cards[i]
 	}
 }
+
+// deck.deal(52)
+//Output: No more cards in the deck
+//{Diamonds of Six}{Hearts of Three}{Spades of Eigth}{Clubs of Four}{Hearts of Two}{Spades of Ace}{Spades of Two}{Clubs of Ace}{Diamonds of Seven}{Spades of Ten}{Clubs of Three}{Hearts of Seven}{Hearts of Jack}{Hearts of Ace}{Diamonds of Three}{Hearts of Four}{Diamonds of Four}{Hearts of Six}{Clubs of Six}{Clubs of Seven}{Diamonds of Jack}{Hearts of Five}{Spades of Three}{Hearts of Queen}{Diamonds of Two}{Clubs of Queen}{Spades of Nine}{Hearts of King}{Diamonds of Ace}{Diamonds of King}{Clubs of Eigth}{Clubs of Five}{Hearts of Nine}{Clubs of Nine}{Hearts of Ten}{Hearts of Eigth}{Diamonds of Nine}{Clubs of Ten}{Spades of Five}{Spades of King}{Clubs of King}{Diamonds of Ten}{Diamonds of Five}{Spades of Jack}{Spades of Queen}{Spades of Seven}{Spades of Four}{Diamonds of Queen}{Diamonds of Eigth}{Clubs of Jack}{Clubs of Two}{Spades of Six}
+
+// deck.deal(10)
+//Output:{Spades of Four}{Spades of Two}{Hearts of Seven}{Diamonds of Four}{Hearts of Two}{Clubs of King}{Spades of Queen}{Spades of King}{Spades of Seven}{Clubs of Ace}
