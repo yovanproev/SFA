37b7cc68f0b63de323fbf8e3444e90c15a397a0a
yovanproev <yovanproev@yahoo.com>
2022-03-06 16:43:09 +0100

    Task 2-Area() complete

--- Week3/Lecture8/Task2/main.go
+++ Week3/Lecture8/Task2/main.go
@@ -1,30 +1,48 @@
 package main
 
-type Shapes []Shape
+import (
+	"fmt"
+	"math"
+)
 
 type Shape interface {
-	Area()
-}
-
-type ReceiverType struct {
-}
-
-type Square struct {
-	NewSquare int
-}
-
-type Circle interface {
 	Area() float64
 }
 
-func (s Shapes) LargestArea() float64 {
-	return 3.14
+// type Shapes []Shape
+// func (s Shapes) LargestArea() float64 {}
+
+// Not sure how to use this type here....?????
+// shape is an interface
+
+type Square struct {
+	width, height float64
+}
+type Circle struct {
+	radius float64
 }
 
-func (r *ReceiverType) Area() float64 {
-	return 3.14
+//The implementation for Area().
+func (s Square) Area() float64 {
+	return s.width * s.height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func measure(s Shape) {
+	fmt.Println("Area", s.Area())
 }
 
 func main() {
+	s := Square{width: 3, height: 4}
+	c := Circle{radius: 5}
 
+	measure(s)
+	measure(c)
 }
+
+// Output:
+// 1 12
+// 1 78.53981633974483
