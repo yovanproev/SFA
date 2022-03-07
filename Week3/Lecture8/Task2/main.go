9082dcb55318208109eb054dfe28e66db301e93e
yovanproev <yovanproev@yahoo.com>
2022-03-07 13:00:03 +0100

    Task2 ready

--- Week3/Lecture8/Task2/main.go
+++ Week3/Lecture8/Task2/main.go
@@ -7,13 +7,34 @@ import (
 
 type Shape interface {
 	Area() float64
+	LargestArea() float64
 }
 
-// type Shapes []Shape
-// func (s Shapes) LargestArea() float64 {}
+type Shapes struct {
+	Shape []Shape
+}
 
-// Not sure how to use this type here....?????
-// shape is an interface
+func (s Shapes) LargestArea() float64 {
+	var largestNumber []float64
+	x := s.Shape
+	for _, s := range x {
+		largestNumber = append(largestNumber, s.Area())
+	}
+
+	fmt.Println("All shapes", largestNumber)
+	var maxNumber float64
+	for _, e := range largestNumber {
+		if e > maxNumber {
+			maxNumber = e
+		}
+	}
+
+	return maxNumber
+}
+
+func (s Shapes) Area() float64 {
+	return 3.14
+}
 
 type Square struct {
 	width, height float64
@@ -31,18 +52,35 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
-func measure(s Shape) {
+func (s Square) LargestArea() float64 {
+	return s.width * s.height
+}
+
+func (c Circle) LargestArea() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func measureLA(s Shape) {
+	fmt.Println("LargestArea", s.LargestArea())
+}
+
+func measureA(s Shape) {
 	fmt.Println("Area", s.Area())
 }
 
 func main() {
-	s := Square{width: 3, height: 4}
-	c := Circle{radius: 5}
+	s := Square{width: 5, height: 4}
+	c := Circle{radius: 2}
 
-	measure(s)
-	measure(c)
+	measureA(s)
+	measureA(c)
+
+	shape1 := []Shape{&Square{width: 5, height: 4}, &Circle{radius: 2}}
+	measureLA(Shapes{shape1})
 }
 
 // Output:
-// 1 12
-// 1 78.53981633974483
+// Area 20
+// Area 12.566370614359172
+// All shapes [20 12.566370614359172]
+// LargestArea 20
